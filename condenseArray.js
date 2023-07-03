4a509718b7db6375af6a25d4f6e3eb1964be88e9
TootEgozy <tootegozy@gmail.com>
2023-07-03 21:38:56 +0300

    WIP sketch to instructions algorithm

--- condenseArray.js
+++ condenseArray.js
@@ -1,25 +1,39 @@
 const sumRepeatingChars = (arr) => {
   const result = [];
   let memory = {};
-  arr.forEach((c) => {
-    if (memory[c]) memory[c] = memory[c]+1; // {A: 2}
+  arr.forEach((c, i) => {
+    if (memory[c]) memory[c] = memory[c]+1;
     else {
       Object.keys(memory).forEach((char) => result.push(`(${char})${memory[char]}`));
       memory = {[c]: 1}
     }
   })
   const lastChar = Object.keys(memory);
-  if (lastChar.length) result.push(lastChar[0]);
+  if (lastChar.length) result.push(`(${lastChar[0]})${memory[lastChar[0]]}`);
   return result;
 }
 
-const condenseArray = (arr) => {
-  // [A, A, B, A, A, B]
-  // [[A, A], B, [A, A], B]
-  // [[[A, A], B], [[A, A], B]]]
-  // {(A)2, (B)1}2
-
-  console.log(sumRepeatingChars('AABAAAAABABBB'.split('')));
+const condenseArray = (charsArray) => {
+  const withoutRepeatingChars = sumRepeatingChars(charsArray);
+  let loop = true;
+  const iterateAndCondense = (arr) => {
+    const res = [];
+    const charsAt = {};
+    arr.forEach((char, i) => {
+      if(charsAt[char]) {
+        const charLastIndex = charsAt[char].indexes[charsAt[char].indexes.length - 1]
+        const gap = i - charLastIndex;
+        const firstSeq = arr.slice(charLastIndex, gap).toString();
+        const secondSeq = arr.slice(i, i + gap).toString();
+        if(firstSeq === secondSeq) charsAt[char].indexes.push(i);
+      } else {
+        charsAt[char] = { indexes: [i] };
+      }
+    });
+    Object.keys(charsAt).forEach((char) => res.splice(charsAt[char], 0, char));
+    return res;
+  }
+  return iterateAndCondense(withoutRepeatingChars);
 }
 
-condenseArray([]);
+console.log(condenseArray('AAABBBAAABBBABABABABAAAABABABABAAAABABABAB'.split('')));
