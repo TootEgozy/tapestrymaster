3ed86069bb3df7619c09b38f8f013a0bfb0993ed
TootEgozy <tootegozy@gmail.com>
2023-07-05 23:01:11 +0300

    WIP instructions algorithm, working for adjacent patterns - debugging

--- condenseArray.js
+++ condenseArray.js
@@ -1,5 +1,3 @@
-
-
 // this function is meant to be run multiple times on the array until there are no more duplicates.
 // the idea is to find all adjacent sequences and once they are converted to special characters, run again and find upper sequences.
 // for example, [(A)3, (B)1, (A)2, (B)1, (A)2, (A)3, (B)1, (A)2, (B)1, (A)2]
@@ -33,13 +31,12 @@ const charObj = {
 // check to see if there are adjacent matching sequences, if so, returns the new seq obj. if not, returns undefined.
 const getAdjSeq = (char, arr, i) => {
   const lastIndex = char.indexes[char.indexes.length - 1];
-  let gap = i - lastIndex;
-  // if(lastIndex === 0) gap = gap - 1;
-  const firstSeq = arr.slice(lastIndex, i).toString();
-  const secondSeq = arr.slice(i, i + gap).toString();
-  if(firstSeq === secondSeq) {
+  const gap = i - lastIndex;
+  const firstSeq = arr.slice(lastIndex, i);
+  const secondSeq = arr.slice(i, i + gap);
+  if(firstSeq.toString() === secondSeq.toString()) {
     return({
-      chars: secondSeq.split(','),
+      chars: secondSeq,
       startIndex: lastIndex,
       endIndex: i + gap - 1,
       repeat: 2,
@@ -55,33 +52,38 @@ const updateDupSeq = (dupSeq, seq) => {
 }
 
 // creates an array of all sequences
-const getSequencesFromMemory = (memory) => {
-  const result = [];
-  Object.keys(memory).forEach((char) => {
-    if(memory[char].sequences) memory[char].sequences.forEach((seq) => result.push(seq));
-  });
-  return result;
-}
+const getSequencesFromMemory = (memory) => Object.keys(memory).filter((char) => memory[char].sequences).map((char) => memory[char].sequences).flat();
 
-const isContainedInRange = (xStart, xEnd, yStart, yEnd) => (xStart >= yStart && xEnd <= yEnd);
+
+// check if a given sequence is wrapping another sequence from the memory array. if so, it's invalid.
+// for example: ACCCBDACCCBD is wrapping CCC,
+// and therefore should be ignored now and condensed in the next recursion.
+const isWrappingSequence = (sequences, seq) => {
+  return !!sequences.find((s) => {
+    return seq.chars.toString().includes(Array.from(Array(s.repeat)).fill(s.chars).toString())
+  });
+}
 
 const isShifted = (xStart, xLength, yStart, yLength) => (xStart >= yStart && xLength === yLength);
 
-const isRedundant = (sequences, seq, i) => {
-  return !!sequences.find((s, sameI) => {
-    // TODO: that's dumb find a different way to do it
-    if(i === sameI) return false;
-    const isContained = isContainedInRange(seq.startIndex, seq.lastIndex, s.startIndex, s.lastIndex);
-    const isSameButShifted = isShifted(seq.startIndex, seq.chars.length, s.startIndex, s.chars.length);
-    return isContained ||isSameButShifted;
+// skip this sequence if it's the same but shifted, or if it's wrapping a nested sequence.
+const skip = (sequences, seq, i) => {
+  const sequencesWithoutSeq = sequences.slice();
+  sequencesWithoutSeq.splice(i, 1);
+  const res = sequencesWithoutSeq.find((s) => {
+      const isWrappingSeq = isWrappingSequence(sequencesWithoutSeq, seq);
+      const isSameButShifted = isShifted(seq.startIndex, seq.chars.length, s.startIndex, s.chars.length);
+      return isWrappingSeq || isSameButShifted;
   });
+  return !!res;
 };
 
 // construct the new array with duplicate chars instead of found repeating chars
 const createCondensedArray = (charsArr, sequences) => {
   const newCharsArr = [...charsArr];
   sequences.forEach((seq, i) => {
-    if(!isRedundant(sequences, seq, i)) {
+    console.log("skip?: "+skip(sequences, seq, i));
+    if(!skip(sequences, seq, i)) {
       let gap = seq.endIndex - seq.startIndex;
       if(seq.startIndex === 0) gap ++;
       const strSeq = seq.chars.toString();
@@ -110,7 +112,6 @@ const condenseArray = (charsArr) => {
       }
       else memory[char] = { indexes: [i] };
     });
-    console.log(memory);
     const sequences = getSequencesFromMemory(memory);
     if (!sequences.length) return charsArr.join(''); // if there are no new sequences, we are done
     return condenseArray(createCondensedArray(charsArr, sequences));
@@ -120,5 +121,5 @@ const input = 'CDABABABADCHJIKLMLMC'.split('');
 const input2 = 'GACDCCCACDCCCGHJ'.split('');
 // first GACD(C)3ACD(C)3GHJ
 // second and final G(ACD(C)3)GHI
-console.log(input2.toString());
+// console.log(input2.toString());
 console.log(condenseArray(input2));
