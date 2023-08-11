4b2e2dbe1eb547f6d10f5f1938e7c5f6378b68e3
TootEgozy <tootegozy@gmail.com>
2023-08-11 22:59:57 +0300

    wip converting to vue

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@
         <input type="number" min="0" id="columns" />
         <label for="rows">rows:</label>
         <input type="number" min="0" id="rows" />
-        <button>Generate</button>
+        <button @click="this.generateTable()">Generate</button>
       </div>
     </div>
+    <div id="table-container"></div>
   </div>
 </template>
 
@@ -24,10 +25,45 @@ export default {
       columnsNumber: undefined,
       generated: false,
       colors: [],
+      colorA: "blue",
+      colorB: "orange",
     };
   },
 
-  methods: {},
+  methods: {
+    toggleColor(event) {
+      const cell = event.target;
+      const cellColor = cell["data-color"];
+      const [newColorType, newColor] =
+        cellColor === "colorA"
+          ? ["colorB", this.colorB]
+          : ["colorA", this.colorA];
+      cell.style.backgroundColor = newColor;
+      cell["data-color"] = newColorType;
+    },
+
+    generateTable() {
+      const columnsElement = document.getElementById("columns");
+      const rowsElement = document.getElementById("rows");
+      const container = document.getElementById("table-container");
+      container.innerHTML = "";
+      const [cols, rows] = [columnsElement.value, rowsElement.value];
+      const table = document.createElement("table");
+      for (let i = 0; i < rows; i++) {
+        const row = table.insertRow();
+        row["order"] = rows - i;
+        row["side"] = (rows - i) % 2 === 0 ? "WS" : "RS";
+        for (let j = 0; j < cols; j++) {
+          const cell = row.insertCell();
+          cell.className = "table-cell";
+          cell["data-color"] = "colorA";
+          cell.style.backgroundColor = this.colorA;
+          cell.onclick = this.toggleColor;
+        }
+      }
+      container.appendChild(table);
+    },
+  },
 };
 </script>
 
@@ -38,5 +74,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  #columns-rows-input {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    table {
+      border-collapse: collapse;
+    }
+    td {
+      border: solid 2px black;
+    }
+    .table-cell {
+      width: 40px;
+      height: 40px;
+    }
+  }
 }
 </style>
--- src/components/DrawingTable.vue
+++ src/components/DrawingTable.vue
@@ -1,3 +1,6 @@
+// I want to write a functional component that receives rows and columns number and generates a table with rows and columns accordingly.
+// then I want this table to only be visible when the generate button is pressed, regenerated every click.
+
 <template>
   <div class="drawing-table-container">
   </div>
