1a50781bd8b9f336e27b14783224571622e28926
TootEgozy <tootegozy@gmail.com>
2023-08-31 23:12:59 +0300

    colors input wip

--- src/components/ColorsInput.vue
+++ src/components/ColorsInput.vue
@@ -1,14 +1,21 @@
 <template v-if="this.colors.length">
 
   <div class="colors-input-container" v-for="(color, i) in colors" :key="i">
-      <label for="color">color{{color.alphabeticalName}}</label>
+      <input
+          type="text"
+          :class="color-name"
+          v-model="colors[i].name"
+      >
+
       <input
           type="color"
-          class="`color${color.alphabeticalName}`"
+          :class="[color.alphabeticalName]"
           v-model="colors[i].RGB"
+          @input="resetColorName"
       >
+    <button @click="removeColor(color)">-</button>
   </div>
-  <button @click="console.log(colors)">log colors</button>
+  <button @click="addNewColor">+</button>
 
 </template>
 
@@ -23,40 +30,56 @@ export default {
   data() {
     return {
       colors: [],
-      newColor: undefined,
       availableLetters: Array.from(Array(26)).map((e, i) => i + 65).map((n) => String.fromCharCode(n)),
       availableColors: namedColors,
     }
   },
 
   methods: {
-
     randomiseIndex(max) {
         return Math.round(Math.random() * max);
     },
-
     getColorOrder() {
       if(!this.colors.length) return 0;
       return this.colors[this.colors.length - 1].order + 1;
     },
-
     generateColor(order) {
       const letter = this.availableLetters.splice(0, 1)[0];
       const colorIndex = this.randomiseIndex(this.availableColors.length - 1);
       const color = this.availableColors.splice(colorIndex, 1)[0];
       return {
         order,
-        alphabeticalName: letter,
-        name: color.name,
+        alphabeticalName: `color${letter}`,
+        name:  color.name,
         RGB: color.RGB,
       }
     },
-
     initialiseColors() {
       this.availableColors = this.availableColors.length ? this.availableColors : namedColors;
       const newColors = [this.generateColor(0), this.generateColor(1)];
       newColors.forEach((color) => this.colors.push(color));
     },
+    resetColorName(e) {
+      const colorIndex = this.colors.findIndex((color) => color.alphabeticalName === e.target.className);
+      if(colorIndex > -1) {
+        const color = this.colors[colorIndex];
+        color.name = undefined;
+        this.colors.splice(colorIndex, 1, [color]);
+      }
+    },
+    removeColor(colorToRemove) {
+      const toRemoveIndex = this.colors.findIndex((color) => {
+        return color.RGB === colorToRemove.RGB && color.alphabeticalName === colorToRemove.alphabeticalName
+      });
+      this.colors.splice(toRemoveIndex, 1);
+    },
+    addNewColor() {
+      const newColor = this.generateColor(this.colors.length > 0 ? this.colors.length : 1);
+      this.colors.push(newColor);
+    },
+    getColors () {
+      return this.colors;
+    }
   },
 
   mounted() {
